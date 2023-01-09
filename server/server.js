61b923981dce193be2331a7086c32f5879f9f96b
abhin <abhinavrathore.ar@gmail.com>
2023-01-09 13:22:49 +0530

    commit 1: for deployment

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log("hi to server");
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+console.log(path.join(__dirname, "build"));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
