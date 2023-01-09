1cc6dac7b70522a481831e0ade93dec09599518d
abhin <abhinavrathore.ar@gmail.com>
2023-01-09 13:59:41 +0530

    deployement 3

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, "build")));
 console.log(path.join(__dirname, "build"));
 
 // This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+// app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
