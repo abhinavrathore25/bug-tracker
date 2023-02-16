77fbe4394808ff493ed1ded77a2c6bd974d3fa5d
abhin <abhinavrathore.ar@gmail.com>
2023-02-16 19:11:33 +0530

    cors headers added in server to allow from any origin

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ const cors = require("cors");
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(bodyParser.urlencoded({
     extended: true
