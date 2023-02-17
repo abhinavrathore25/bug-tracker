05958b63a569c90c52f01e8a98a53bdeeb8ee75f
abhin <abhinavrathore.ar@gmail.com>
2023-02-17 17:25:11 +0530

    allow credentials set to false

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', false);
     next();
 });
 
